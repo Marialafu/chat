aaa99957e1c085ec49c57831d7a6265724049949
Marialafu <lafuentelhmaria@gmail.com>
2025-06-08 11:58:12 +0200

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,45 @@
-import { useEffect, useState } from 'react';
-import { GlobalStyles } from './styles/GlobalStyles';
-import Form from './components/form/Form';
-import { io } from 'socket.io-client';
+// import { useEffect, useState } from 'react';
+// import { GlobalStyles } from './styles/GlobalStyles';
+// import Form from './components/form/Form';
+// import { io } from 'socket.io-client';
 
-const socket = io();
+// const socket = io();
 
-const App = () => {
-	const [isConnected, setIsConnected] = useState(false);
-	const [message, setMessage] = useState([]);
+// const App = () => {
+// 	const [isConnected, setIsConnected] = useState(false);
+// 	const [message, setMessage] = useState([]);
 
-	useEffect(() => {
-		socket.connect();
+// 	useEffect(() => {
+// 		socket.connect();
 
-		socket.on('connect', () => setIsConnected(true));
-		socket.on('disconnect', () => setIsConnected(false));
+// 		socket.on('connect', () => setIsConnected(true));
+// 		socket.on('disconnect', () => setIsConnected(false));
 
-		socket.on('messageEmited', data => {
-			setMessage([...message, data]);
-		});
+// 		socket.on('messageEmited', data => {
+// 			setMessage([...message, data]);
+// 		});
 
-		return () => {
-			socket.disconnect();
-		};
-	}, []);
+// 		return () => {
+// 			socket.disconnect();
+// 		};
+// 	}, []);
 
-	return (
-		<>
-			<GlobalStyles />
-			<h1>CHAT:</h1>
-			<ul>mensajes; {message}</ul>
-			<Form message={message} setMessage={setMessage} />
-		</>
-	);
-};
+// 	return (
+// 		<>
+// 			<GlobalStyles />
+// 			<h1>CHAT:</h1>
+// 			<ul>mensajes; {message}</ul>
+// 			<Form message={message} setMessage={setMessage} />
+// 		</>
+// 	);
+// };
 
-const exportData = async setData => {
-	const response = await fetch('http://localhost:3000/');
-	const data = await response.json();
-	console.log(data);
+// const exportData = async setData => {
+// 	const response = await fetch('http://localhost:3000/');
+// 	const data = await response.json();
+// 	console.log(data);
 
-	//setData(data);
-};
+// 	//setData(data);
+// };
 
-export default App;
+// export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,5 @@
-const express = require('express');
-const { createServer } = require('node:http')
-const { Server} = require('socket.io')
-
-const app = express();
-const server = createServer(app)
-const io = new Server(server)
-
-const port = 3000;
-const cors = require('cors');
-const initialRoutes = require('./routes/initial.routes');
+const server = HTMLOutputElement.createServer();
+const port = 3000
 
 const corsOptions = {
   origin: '*', // Orígenes permitidos (cuando esté en un dominio real, lo cambiaremos por ese dominio)
@@ -16,24 +7,24 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'] // Headers permitidos
 };
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use('/', initialRoutes);
+const io = require('socket.io')(server, {...corsOptions})
 
 io.on('connection', (socket) => {
   console.log(`Usuario conectado: ${socket.id}`);
 
-  socket.on('message', (data) => {
-    io.emit('messageEmited', data)
-  })
+  // socket.on('message', (data) => {
+  //   io.emit('messageEmited', data)
+  // })
 
-  socket.on('disconnect', () => {
-    io.emit('disconnectedMessage', `Usuario desconectado: ${socket.id}`)
-    console.log(`Usuario desconectado: ${socket.id}`);
+  // socket.on('disconnect', () => {
+  //   io.emit('disconnectedMessage', `Usuario desconectado: ${socket.id}`)
+  //   console.log(`Usuario desconectado: ${socket.id}`);
     
-  })
+  // })
 })
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+server.listen(port)
